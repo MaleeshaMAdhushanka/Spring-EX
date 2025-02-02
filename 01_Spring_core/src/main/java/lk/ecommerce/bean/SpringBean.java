f22cea1192f980bcc70140f783d8e241557e071e
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-02-02 13:01:47 +0530

    Simple  Spring Application Structure using @Confuguration , @ComponetScan, @Componet

---
+++ 01_Spring_core/src/main/java/lk/ecommerce/bean/SpringBean.java
@@ -0,0 +1,10 @@
+package lk.ecommerce.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean {
+    public SpringBean(){
+        System.out.println("Spring Bean Object Created");
+    }
+}
---
+++ 01_Spring_core/src/main/java/lk/ecommerce/config/AppConfig.java
@@ -0,0 +1,12 @@
+package lk.ecommerce.config;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ecommerce.bean")
+public class AppConfig {
+
+
+}
