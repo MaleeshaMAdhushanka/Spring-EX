d04c774beece34e7fc676d88ec1feff56fdbf9e8
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-02-06 23:46:08 +0530

    using controller get view

--- 08_Spring_Web_MVC/src/main/java/lk/ecommerce/WebAppInitializer9.java
+++ 08_Spring_Web_MVC/src/main/java/lk/ecommerce/WebAppInitializer9.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class WebAppInitializer9 extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
--- 08_Spring_Web_MVC/src/main/java/lk/ecommerce/controller/TestController.java
+++ 08_Spring_Web_MVC/src/main/java/lk/ecommerce/controller/TestController.java
@@ -1,15 +1,21 @@
 package lk.ecommerce.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+//Using view returns
+@Controller
 @RequestMapping("test")
 public class TestController {
 
+
+//    Handler method, Mapping method
     @GetMapping
     public String text(){
-        return "test";
+//        return "test";
+        return "index";
     }
 }
