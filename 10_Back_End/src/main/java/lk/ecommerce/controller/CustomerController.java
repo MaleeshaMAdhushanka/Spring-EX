a2c2a8754310951b308cc99ef9de98b9496ad07e
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-02-19 10:49:33 +0530

    Creating Front_end , Index,html, Creating js saveCustomer, refreshTable, clearFileds, loadCustomer

--- 10_Back_End/src/main/java/lk/ecommerce/controller/CustomerController.java
+++ 10_Back_End/src/main/java/lk/ecommerce/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:63342")
+
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
